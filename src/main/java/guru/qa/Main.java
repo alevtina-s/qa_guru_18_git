983c9510d7edb49ffdefd2e0abb7c21ac9ff25d6
Alevtina Sergienko <sergienkoalevtina@gmail.com>
2024-03-17 16:28:50 +0300

    add hello method

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,6 +2,10 @@ package guru.qa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Pavel!");
+        System.out.println(getMessage());
+    }
+
+    public static String getMessage() {
+        return "Hello programmers!";
     }
 }
